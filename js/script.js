c3941abf52961a9336f7d09df6a3a48dd88fb7cf
SarahOh92 <201124081oek@gmail.com>
2026-08-28 16:46:43 +0900

    메인페이지 about 섹션 업데이트 및 css 수정

--- js/script.js
+++ js/script.js
@@ -41,8 +41,8 @@ $(function(){
     gsap.timeline({
         scrollTrigger: {
             trigger: '.con01', // 트리거 대상
-            start: '0% 80%', // 트리거 대상의 0%와 브라우저의 80%가 만날때 애니매이션이 시작됨
-            end: '100% 100%', // 트리거 대상의 100%와 브라우저의 100%가 만날때 애니메이션이 종료됨
+            start: '0% 0%', // 트리거 대상의 0%와 브라우저의 80%가 만날때 애니매이션이 시작됨
+            end: '0% 20%', // 트리거 대상의 100%와 브라우저의 100%가 만날때 애니메이션이 종료됨
             scrub: 1, // GSAP ScrollTrigger의 이벤트가 스크롤이 사용될 때만 재생되도록 만들어주는 속성
             // markers: true // 시작위치와 끝위치를 확인가능하게 하는 마커
         }
@@ -51,22 +51,22 @@ $(function(){
     .to('.wrap', {backgroundColor: '#ffffff', color: '#000000', ease:'none', duration: 5}, 0)
     .to('.svgani path', {stroke: '#000000', ease:'none', duration:5}, 0 )
     .to('.scroll', {opacity: '0', ease:'none', duration:5}, 0 )
-    .fromTo('.video_wrap video', {'clip-path': 'inset(60% 60% 60% 60% round 30%)'}, {'clip-path': 'inset(0% 0% 0% 0% round 0%)', ease: 'none', duration: 10}, 0)
+    // .fromTo('.video_wrap video', {'clip-path': 'inset(60% 60% 60% 60% round 30%)'}, {'clip-path': 'inset(0% 0% 0% 0% round 0%)', ease: 'none', duration: 10}, 0)
 
 
-    // .con02 gsap animation
+    // .con03 gsap animation
     // title 글자 애니메이션
     gsap.timeline({
         scrollTrigger : {
-            trigger : ".con02",
+            trigger : ".con03",
             start : '0% 100%',
             end : '0% 20%',
             scrub : 1,
             // markers : true
         }
     })
-    .fromTo('.con02 .title .a', {x: '-100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
-    .fromTo('.con02 .title .b', {x: '100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
+    .fromTo('.con03 .title .a', {x: '-100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
+    .fromTo('.con03 .title .b', {x: '100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
 
     // work_list가 나타날때 배경색 검정으로
     gsap.timeline({
@@ -81,7 +81,7 @@ $(function(){
     .to('.wrap', {backgroundColor: "#000000", color: "#ffffff", ease: "none", duration: 5}, 0)
 
     // title 글자 position: fixed 적용
-    .to('.con02 .title', {position: 'fixed', ease: 'none', left:'0', top: '0', width: '100%', duration: 5}, 0)
+    .to('.con03 .title', {position: 'fixed', ease: 'none', left:'0', top: '0', width: '100%', duration: 5}, 0)
 
     // work_list에 margin-top을 적용해서 애니를 자연스럽게 적용
     .fromTo('.work_list', {margin: '0 auto'}, {margin: '100vh auto 0', position: 'relative', zIndex: '10', duration: 1}, 0)
@@ -96,13 +96,13 @@ $(function(){
             scrub: 1
         }
     })
-    .to('.con02 .title .a', {x: '-100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
-    .to('.con02 .title .b', {x: '100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
+    .to('.con03 .title .a', {x: '-100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
+    .to('.con03 .title .b', {x: '100%'}, {x: '0%', ease: 'none', duration: 5}, 0)
 });
 
 // simplyScroll
 $(function(){
-    $(".con03 .list").simplyScroll({
+    $(".con02 .list").simplyScroll({
         speed: 4,
         pauseOnHover: false,
         pauseOnTouch: false,
